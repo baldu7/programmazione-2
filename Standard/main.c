3a724d72e97c1138b404e78bd071b8c18749ddf8
Daniele <balducellidaniele@yahoo.it>
2014-12-09 10:16:09 +0100

    Create main.c

---
+++ Standard/main.c
@@ -0,0 +1,20 @@
+//Librerie
+#define <stdio.h>
+
+/*==============================================================================
+Autore      : Daniele Balducelli	Matricola:
+Programma   :
+Data        : 01/12/2014
+To Do:
+------------------------------------------------------------------------------*/
+
+//prototipi funzioni
+
+
+//Programma principale
+main(){
+//dichiazione delle variabili
+
+}
+
+//Funzioni
